b4046b355c14e9d5233720f39de3637a0a027bb8
unknown <ns651143@gmail.com>
2024-11-15 10:00:14 +0530

    route

--- src/app.js
+++ src/app.js
@@ -13,5 +13,11 @@ app.use(express.urlencoded({extended:true,limit:'20kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+import userRouter from "./routes/user.router.js"
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
 
 export {app}
